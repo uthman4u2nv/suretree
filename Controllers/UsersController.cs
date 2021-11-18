743c918eb784db80dd3694a0d03e1e47fe00590f
Ajagbe Uthman <uthman4u2nv@yahoo.com>
2021-11-18 12:45:18 +0100

    First Push

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using suretree.Services;
 
 namespace suretree.Controllers
 {
@@ -11,5 +12,31 @@ namespace suretree.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        public readonly IPerson pe;
+
+            public UsersController(IPerson p)
+        {
+            pe = p;
+        }
+        
+        [HttpGet("person/{int Id}")]
+        public async Task<ActionResult> GetPerson(int Id)
+        {
+            return Ok(pe.GetById(Id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Person(User u)
+        {
+            return Ok(pe.Insert(u));
+        }
+
+        [HttpGet("person")]
+        public async Task<IEnumerable<User>> ListUser()
+        {
+            return Ok(pe.list());
+        }
+
+
     }
 }
--- User.cs
+++ User.cs
@@ -5,7 +5,33 @@ using System.Threading.Tasks;
 
 namespace suretree
 {
-    public class User
+    public class User : Services.IPerson
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        List<User> u = new List<User>();
+        
+
+        public User GetById(int Id)
+        {
+            return u.FirstOrDefault(m => m.Id == Id);
+        }
+
+        public int Insert(User u)
+        {
+            u.Id = 10;
+            u.Name = "Ajagbe";
+            u.Age = 100;
+            u.DateOfBirth = DateTime.Now;
+            
+        }
+
+        public List<User> list()
+        {
+            return u;
+        }
     }
 }
