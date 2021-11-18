74f9f5a4c2d9431c5077cd140ce93108c22d207a
Ajagbe Uthman <uthman4u2nv@yahoo.com>
2021-11-18 12:38:12 +0100

    First Push

---
+++ Services/IPerson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace suretree.Services
+{
+    public interface IPerson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        User GetById(int Id);
+        int Insert(User u);
+        List<User> list();
+
+    }
+}
